c141da244d142b17f6bb076d693acad39f2bd6c6
Nikola Markovic <markovi3c.nikola@gmail.com>
2023-01-17 15:21:01 +0100

    Learning.

---
+++ 6-mdn-loops-1/script.js
@@ -0,0 +1,8 @@
+const myArray = ['tomatoes', 'chick peas', 'onions', 'rice', 'black beans'];
+const list = document.createElement('ul');
+
+for (const items of myArray) {
+}
+
+const section = document.querySelector('section');
+section.appendChild(list);
---
+++ 6prep/script.js
@@ -0,0 +1,24 @@
+const link = document.querySelectorAll('a');
+
+for (let i = 0; i < link.length; i++) {
+  link[i].textContent = 'Mozilla Developer Network';
+  link[i].href = 'https://developer.mozilla.org';
+  link[i].style.color = 'red';
+}
+
+const sect = document.querySelector('section');
+
+const para = document.createElement('p');
+para.textContent = 'Well, this is a new paragraph.';
+sect.appendChild(para);
+
+const text = document.createTextNode(
+  ' -- the premier source for webdev knowledge.'
+);
+
+const linkPara = document.querySelector('p');
+linkPara.appendChild(text);
+
+sect.appendChild(linkPara);
+
+link[1].remove();
