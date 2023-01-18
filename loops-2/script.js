bdcb1be88ab1e8958d3ea899bf98e86c883dde7c
Nikola Markovic <markovi3c.nikola@gmail.com>
2023-01-18 14:57:10 +0100

    Finish loops-2

---
+++ loops-2/script.js
@@ -0,0 +1,23 @@
+const nameToSearch = 'Mustafa';
+const para = document.createElement('p');
+
+const phonebook = [
+  { name: 'Chris', number: '1549' },
+  { name: 'Li Kang', number: '9634' },
+  { name: 'Anne', number: '9065' },
+  { name: 'Francesca', number: '3001' },
+  { name: 'Mustafa', number: '6888' },
+  { name: 'Tina', number: '4312' },
+  { name: 'Bert', number: '7780' },
+  { name: 'Jada', number: '2282' },
+];
+
+const section = document.querySelector('section');
+//section.appendChild(para);
+
+for (let i = 0; i < phonebook.length; i++) {
+  if (nameToSearch === phonebook[i].name) {
+    console.log(phonebook[i].number);
+    break;
+  }
+}
