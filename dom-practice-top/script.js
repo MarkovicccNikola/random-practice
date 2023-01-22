fe4de2909b39f382b1cf2a546c5777a931956cfa
Nikola <markovi3c.nikola@gmail.com>
2023-01-22 10:16:09 +0100

    Done.

--- dom-practice-top/script.js
+++ dom-practice-top/script.js
@@ -31,3 +31,11 @@ btn.addEventListener('click', function (e) {
     console.log(e);
     e.target.style.background = 'blue';
 });
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        alert(button.id);
+    })
+})
